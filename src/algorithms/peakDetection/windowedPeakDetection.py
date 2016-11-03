ee9ad1cf541841df7f387854d6218f12e562dc20
jamiebrynes7 <jamiebrynes7@gmail.com>
2016-11-03 10:42:38 +0000

    chg: Algorithm uses file location. Added debug prints.

--- src/algorithms/peakDetection/windowedPeakDetection.py
+++ src/algorithms/peakDetection/windowedPeakDetection.py
@@ -9,9 +9,9 @@
 #       peak 'score' function calculator.
 #
 # ======================================================================== #
+from src import utils
 
-from src.constants import Constants
-
+from src.infra.inputPipe import InputPipe
 from src.infra.queue import Queue
 
 from src.algorithms.peakDetection.preProcessing import WpdPreProcessor
@@ -26,7 +26,7 @@ class Wpd:
 
     # Constructor for the object.
     # @args :
-    #   1. queue - the input data stream queue from inputPipe
+    #   1. filelocation - location of the accelerometer_test.csv and stepcounter.csv
     #   2. preProcessingParams - parameters for the preprocessor, see preProcessing.py for docs
     #   3. windowType - type of windowing for the smoothing process. See constants.py for list.
     #   4. windowParams - parameters for the preprocessor, see relevant windowing function for docs.
@@ -34,12 +34,13 @@ class Wpd:
     #   6. peakFuncParams - parameters for the peak scoring function, see relevant scoring function for docs.
     #   7. peakDetectorParams - parameters for the peak detector, see peakDetector.py for docs.
     #   8. postProcessingParams - parameters for post processing, see postProcessing.py for docs.
-    def __init__(self, queue, preProcessingParams, windowParams, peakFuncParams, peakDetectorParams, postProcessingParams):
+    def __init__(self, filelocation, preProcessingParams, windowParams, peakFuncParams, peakDetectorParams, postProcessingParams):
 
         self.name = 'wpd'
+        self.filelocation = filelocation
 
         # Internal queues for data flow
-        self.inputQueue = queue
+        self.inputQueue = Queue()
         self.dataQueue = Queue()
         self.smoothedDataQueue = Queue()
         self.peakScores = Queue()
@@ -52,8 +53,10 @@ class Wpd:
         self.peakScoreData = []
         self.peakData = []
         self.confirmedPeaks = []
+        self.steps = []
 
         # Internal 'worker threads' in the form of objects
+        self.pipe = InputPipe(self.filelocation+'accelerometer.csv', self.inputQueue)
         self.preProcessing = WpdPreProcessor(preProcessingParams, self.inputQueue, self.data, self.dataQueue)
         self.smoothingFilter = SmoothingFilter(windowParams, self.dataQueue, self.preprocessData, self.smoothedDataQueue)
         self.peakScorer = PeakScorer(peakFuncParams, self.smoothedDataQueue, self.smoothedData, self.peakScores)
@@ -63,6 +66,7 @@ class Wpd:
     # Start algorithm signal, kicks off all the worker threads for the various stages
     def start(self):
 
+        self.pipe.start()
         self.preProcessing.start()
         self.smoothingFilter.start()
         self.peakScorer.start()
@@ -72,11 +76,35 @@ class Wpd:
     # Stop algorithm signal, halts all the worker threads after the current operation
     def stop(self):
 
+        self.pipe.start()
         self.preProcessing.stop()
         self.smoothingFilter.stop()
         self.peakScorer.stop()
         self.peakDetection.stop()
         self.postProcessing.stop()
+
+    def getStatus(self):
+
+        return 'Input Data: ' + str(len(self.inputQueue)) \
+               + ' Pre-Processed Data: ' + str(len(self.dataQueue)) \
+               + ' Smoothed Data: ' + str(len(self.smoothedDataQueue)) \
+               + ' Peak Scores: ' + str(len(self.peakScores)) \
+               + ' Peaks: ' + str(len(self.peaks)) \
+               + ' Confirmed Peaks: ' + str(len(self.confirmedPeaks))
+
+    def getCsvStatus(self):
+
+        return str(len(self.inputQueue)) + ',' \
+               + str(len(self.dataQueue)) + ',' \
+               + str(len(self.smoothedDataQueue)) \
+               + ',' + str(len(self.peakScores)) \
+               + ',' + str(len(self.peaks)) \
+               + ',' + str(len(self.confirmedPeaks))
+
+    def extractSteps(self):
+
+        timeData = {'scale': self.preProcessing.ts_factor, 'offset': self.preProcessing.startTime}
+        self.steps = utils.loadStepCsv(self.filelocation + 'stepcounter.csv', timeData)
 
     # Check if the algorithm is done
     def isDone(self):
