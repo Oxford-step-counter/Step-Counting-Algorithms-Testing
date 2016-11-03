757cd33f958d62e1d2113b1cdb90f84cc2e03397
jamiebrynes7 <jamiebrynes7@gmail.com>
2016-11-03 10:49:14 +0000

    chg: Added mean diff peak scoring function

--- src/algorithms/peakDetection/peakFuncs.py
+++ src/algorithms/peakDetection/peakFuncs.py
@@ -51,10 +51,52 @@ class PeakScorer(WorkerThread):
                     self.outputQueue.enqueue(new_dp)
                     self.window.dequeue()
 
+    def meanDiff(self):
+        while self.active:
+            if not self.inputQueue.isEmpty():
+
+                # Get next data point
+                dp = self.inputQueue.dequeue()
+
+                # Special case handling for end data point.
+                if dp == 'end':
+                    self.completed = True
+                    self.active = False
+                    self.outputQueue.enqueue('end')
+                    return
+
+                # Add data point to list and queue
+                self.data.append(dp)
+                self.window.enqueue(dp)
+
+                # Once we reach the window size, do some processing!
+                if len(self.window) == self.windowSize:
+
+                    # Calculate peak score
+                    midPoint = int(self.windowSize / 2)
+                    diffLeft = 0
+                    diffRight = 0
+
+                    # Find max difference on left
+                    for i in range(0, midPoint):
+                        value = self.window[midPoint].mag - self.window[i].mag
+                        diffLeft += value
+
+                    # Find max difference on right
+                    for i in range(midPoint + 1, len(self.window)):
+                        value = self.window[midPoint].mag - self.window[i].mag
+                        diffRight += value
+
+                    # Calculate peak score and create a new point
+                    avg = (diffLeft + diffRight) / (self.windowSize - 1)
+                    new_dp = Sds(self.window[midPoint].time, avg, self.window[midPoint].mag)
+                    self.outputQueue.enqueue(new_dp)
+                    self.window.dequeue()
+
     # Constructor for the object
     # @args:
     #   1. params - dictionary containing relevant parameters
-    #       a. 'window_size_md' - size of the window for maxDiff
+    #       a. 'window_size' - size of the window for maxDiff
     #       b. 'type' - the type of scorer to use
     #   2. smoothData - queue containing smoothed data
     #   3. smoothDataList - list containing smoothed data
@@ -72,12 +114,14 @@ class PeakScorer(WorkerThread):
         self.window = Queue()
 
         # Parameter unpacking
-        self.windowSize = params['window_size_md']
+        self.windowSize = params['window_size']
         self.typ = params['type']
 
         # Assign target
         if self.typ == 'max_diff':
             self.target = self.maxDiff
+        elif self.typ == 'mean_diff':
+            self.target = self.meanDiff
         else:
             raise Exception('Unknown peak scorer type: ' + self.typ)
 
